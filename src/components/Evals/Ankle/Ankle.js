88de068e34558c2df49d29d83e207bbd8441c90c
hseymour <hseymour@skillstorm.com>
2022-11-30 17:49:31 -0800

    basic react app; cleaned out unused files and added basic browser router

---
+++ src/components/Evals/Ankle/Ankle.js
@@ -0,0 +1,9 @@
+function Ankle() {
+    return (
+      <div className="ankleContainer">
+        <h1>It's me! Ankle!</h1>
+      </div>
+    );
+  }
+  
+  export default Ankle;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <div className="homeContainer">
+        <h1>It's me! Home!</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
