93ed0a59620686fe1371fe98e53a5194188fb74c
hseymour <hseymour@skillstorm.com>
2022-11-30 17:53:04 -0800

    renamed files to index and updated imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './components/Home/Home';
-import Ankle from './components/Evals/Ankle/Ankle'
+import Home from './components/Home';
+import Ankle from './components/Evals/Ankle'
 
 function App() {
   return (
